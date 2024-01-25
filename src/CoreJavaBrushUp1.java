42679ca65cbf217e2b90c331b224f711b1ba0d18
Prasad <prasadabnave8989@gmail.com>
2024-01-25 10:41:22 +0530

    First Commit

---
+++ src/CoreJavaBrushUp1.java
@@ -0,0 +1,23 @@
+
+public class CoreJavaBrushUp1 {
+
+	public static void main(String[] args) {
+	
+
+		
+		int myNum = 5;
+		String website = "Rahul Shetty";
+		char letter = 'P';
+		double dec = 5.99;
+		boolean myCard = true;
+		
+		System.out.println(myNum + " is a Number ");
+		System.out.println(website + " is the Best Teacher");
+		System.out.println(letter + " is the silent word in pnemonia");
+		System.out.println(dec + " is the double number");
+		System.out.println(myCard);
+		
+		
+	}
+
+}
